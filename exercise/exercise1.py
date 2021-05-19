d2eef8a6b35a4f303555111f3a08640617020c28
McKay Farley <macfarley5@gmail.com>
2021-05-19 09:25:39 -0700

    exercise 1

---
+++ exercise/exercise1.py
@@ -0,0 +1,17 @@
+'''
+Given a list of paths:
+
+    /path/to/foo/bar/baz
+    /path/to/foo
+    /path/to/foobar/baz
+
+Return the longest common path prefix among all paths. In this case, it should return:
+
+    /path/to
+'''
+def common_prefix(paths):
+    '''
+    @param paths: List[str]
+    @return: str
+    '''
+    pass
